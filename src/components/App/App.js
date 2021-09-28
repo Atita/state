f5c13b414911af14a74ddb10448f8ca236a2f5c5
atita <anaguasoft@gmail.com>
2021-09-29 00:02:51 +0300

    :octo: added product state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Product from '../Product/Product';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  
+    return <Product />
+ 
 }
 
 export default App;
